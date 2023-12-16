f39269c9331c2233d39a197c383416bdfdf7879f
adimaniac <adityabhigade31@gmail.com>
2023-12-16 23:02:20 +0530

    worked on styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        <h2>AdityaRealEstate</h2>
+    <div className='flex justify-between p-4 bg-amber-400'>
+        <h2 className='flex font-serif font-bold text-slate-700 text-xl'>AdityaRealEstate</h2>
         <div>
-            <input type="text" placeholder="Search"/>
+            <input type="text" placeholder="Search" className='border-2 rounded-md border-slate-200'/>
         </div>
-        <div>
-           <h4>Home</h4>
-           <h4>About</h4>
-           <h4>SignIn</h4> 
+        <div className='flex justify-start gap-2'>
+           <h4 className="font-serif font-medium text-slate-700 text-lg cursor-pointer hidden md:block">Home</h4>
+           <h4 className='font-serif font-medium text-slate-700 text-lg cursor-pointer hidden md:block'>About</h4>
+           <h4 className='font-serif font-medium text-slate-700 text-lg cursor-pointer'>SignIn</h4> 
         </div>
     </div>
   )
