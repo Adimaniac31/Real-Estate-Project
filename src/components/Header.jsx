6f434161eb8a05fc90806e743585f785e26b0d13
adimaniac <adityabhigade31@gmail.com>
2023-12-17 13:26:11 +0530

    fixed a bit of responsive error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex justify-between p-4 bg-amber-400'>
-        <h2 className='flex font-serif font-bold text-slate-700 text-xl'>AdityaRealEstate</h2>
+        <h2 className='flex font-serif font-bold text-slate-700 text-sm sm:text-xl'>AdityaRealEstate</h2>
         <div>
-            <input type="text" placeholder="Search" className='border-2 rounded-md border-slate-200'/>
+            <input type="text" placeholder="Search" className='border-2 rounded-md border-slate-200 w-32 md:w-auto'/>
         </div>
         <div className='flex justify-start gap-2'>
            <h4 className="font-serif font-medium text-slate-700 text-lg cursor-pointer hidden md:block">Home</h4>
            <h4 className='font-serif font-medium text-slate-700 text-lg cursor-pointer hidden md:block'>About</h4>
-           <h4 className='font-serif font-medium text-slate-700 text-lg cursor-pointer'>SignIn</h4> 
+           <h4 className='font-serif font-medium text-slate-700 text-sm sm:text-lg cursor-pointer'>SignIn</h4> 
         </div>
     </div>
   )
