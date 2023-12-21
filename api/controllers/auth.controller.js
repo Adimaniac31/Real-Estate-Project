08fd1a35123d0163aae1622ed83f9e97670e01d4
adimaniac <adityabhigade31@gmail.com>
2023-12-21 14:33:19 +0530

    create middleware and a function which gives custom error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup = async (req,res) =>{
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req,res,next) =>{
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({ username, email, password: hashedPassword });
@@ -9,6 +10,6 @@ export const signup = async (req,res) =>{
     res.status(201).json(newUser);
     }
     catch(err){
-        res.status(500).json(err.message);
+        next(err);
     }
 };
