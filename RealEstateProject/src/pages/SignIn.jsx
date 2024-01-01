5742ceea9bc0f130e9b5014c8efe70f7248b13e9
adimaniac <adityabhigade31@gmail.com>
2024-01-01 13:40:37 +0530

    create sigin page

--- RealEstateProject/src/pages/SignIn.jsx
+++ RealEstateProject/src/pages/SignIn.jsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
-
+import signUpImg from "../assets/signUpImg.webp"
+import { Link,useNavigate } from 'react-router-dom'
 const SignIn = () => {
-  return (
+  
+  const [formData,setFormData] = useState({});
+  const [error,setError] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value
+      });
+  };
+  console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      setLoading(true);
+      const res = await fetch('/api/auth/signin',{
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success === false){
+          setLoading(false);
+          setError(data.message);
+          return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/');
+    }
+    catch(error){
+        setLoading(false);
+        setError(error.message);
+    }
+
+  };
+
+  return ( 
     <div>
-        <Header />
+      <Header />
+      <div className='flex justify-center align-center w-full h-auto mt-24'>
+        <div className='hidden md:flex'>
+          <img src={signUpImg} className='border-25 rounded-lg'/>
+        </div>
+        <form className='flex-col flex justify-center align-center text-center mx-24 gap-6 font-serif font-normal text-base' onSubmit={handleSubmit}>
+          <h1 className='font-semibold text-4xl'>Sign In</h1>
+          <input placeholder='E-mail' type="email" id='email' onChange={handleChange}></input>
+          <input placeholder='Password' type="password" id='password' onChange={handleChange}></input>
+          <button disabled={loading} className='border-2 rounded-sm bg-orange-500 border-amber-600'>{loading ? 'loading...' : 'Sign In'}</button>
+          <span>Dont Have an account?
+            <Link to='/sign-up' className='text-cyan-400'> Sign Up</Link>.
+          </span>
+          {error && <p className='text-rose-600'>{error}</p>}
+        </form>
+      </div>
     </div>
   )
 }
