43459ae01c7c8fa87819e805be2dec1923fa64ef
adimaniac <adityabhigade31@gmail.com>
2024-01-04 14:08:08 +0530

    add redux toolkit

--- RealEstateProject/src/pages/SignIn.jsx
+++ RealEstateProject/src/pages/SignIn.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
-import Header from '../components/Header'
-import signUpImg from "../assets/signUpImg.webp"
-import { Link,useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import Header from '../components/Header';
+import signUpImg from "../assets/signUpImg.webp";
+import { Link,useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
+
 const SignIn = () => {
   
   const [formData,setFormData] = useState({});
-  const [error,setError] = useState(null);
-  const [loading,setLoading] = useState(false);
+  const {loading,error} = useSelector ((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
       setFormData({
         ...formData,
@@ -19,7 +22,7 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try{
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',{
         method: 'POST',
         headers:{
@@ -29,17 +32,14 @@ const SignIn = () => {
       });
       const data = await res.json();
       if(data.success === false){
-          setLoading(false);
-          setError(data.message);
+          dispatch(signInFailure(data.message));
           return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate('/');
     }
     catch(error){
-        setLoading(false);
-        setError(error.message);
+        dispatch(signInFailure(error.message));
     }
 
   };
