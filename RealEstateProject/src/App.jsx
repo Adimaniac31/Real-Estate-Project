ea554244ebc7c1e65deecdf53de91f042e34fc9d
adimaniac <adityabhigade31@gmail.com>
2024-04-25 01:30:46 +0530

    fixed App.jsx file for About

--- RealEstateProject/src/App.jsx
+++ RealEstateProject/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
-import About from './pages/about';
+import About from './pages/About';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
